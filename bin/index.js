95620e95be74f408687fe9e7488f93ef8723c470
Handzic Haris <handzicharis62@yahoo.com>
2023-11-16 16:22:39 +0100

    Updated node

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,11 @@ const { execSync } = require("child_process");
 const args = process.argv.slice(2);
 if (args.length === 0) return;
 
-execSync(
-  `npx react-native@latest init ${args[0]} --template @haris523/react-native-starter`
-);
+try {
+  execSync(
+    `npx react-native@latest init ${args[0]} --template @haris523/react-native-starter`,
+    { stdio: "inherit" }
+  );
+} catch (err) {
+  console.log(err);
+}
